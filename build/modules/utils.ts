9f5fd5d7ee91969d5a292d69792c91c3de501867
Infant Frontender <newbornfrontender@gmail.com>
2018-07-26 13:51:36 +0300

    Had been added webpack utils

---
+++ build/modules/utils.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+import { getResolvePath } from './parts/resolve';
+import { getConsoleLog } from './parts/log';
+
+export {
+  getResolvePath as resolve,
+  getConsoleLog as log,
+};
