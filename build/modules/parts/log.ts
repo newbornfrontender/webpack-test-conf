14898bfc8e3534c2c5cffb11750d7f3d77cdb6a8
Infant Frontender <newbornfrontender@gmail.com>
2018-07-26 18:18:06 +0300

    Added log for mode

--- build/modules/parts/log.ts
+++ build/modules/parts/log.ts
@@ -2,10 +2,25 @@
 
 import chalk from 'chalk';
 
-const getConsoleLog = ({ prop, msg }: any) => {
-  if (prop !== undefined) {
-    return console.log(chalk.blue(msg));
-  };
+const env = process.env.NODE_ENV;
+
+const getConsoleLog = {
+  mode: () => {
+    if (env === undefined) {
+      return console.log(chalk.red(
+        'Oops! Something went wrong. Mode are undefined. ' +
+        'Webpack running in "development" mode!'
+      ));
+    };
+
+    return console.log(chalk.green(`Webpack running in "${env}" mode!`));
+  },
+
+  props: ({ prop, msg }: any) => {
+    if (prop !== undefined) {
+      return console.log(chalk.blue(msg));
+    };
+  },
 };
 
 export { getConsoleLog };
